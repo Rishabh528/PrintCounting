31bf80a96fbd789029bb827b76b3e058d366b0ae
Rishabh528 <rishabh.agg96@gmail.com>
2020-01-20 12:36:05 +0530

    added time feature to show time taken to print values

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.onload = function() {
     let in_num = document.getElementById('in_num');
     let btn = document.getElementById('btn');
-
+    let time = document.getElementById('time');
     function print(){
+
+        let start = new Date().getTime();
+
         let counting = document.getElementById('counting');
         counting.innerText = ''
         for(let i=1;i<=parseInt(in_num.value);i++){
@@ -10,6 +13,11 @@ window.onload = function() {
             number.innerText = `${i}`;
             counting.appendChild(number);
         }
+
+        let end = new Date().getTime();
+        let total_time = end - start;
+        time.innerText = 'Time for print counting is:' + total_time + 'milliseconds';
+        in_num.value = ''
     }
 
     btn.onclick = print
@@ -21,3 +29,4 @@ window.onload = function() {
       })
     
 }
+
